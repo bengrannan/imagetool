b6b146c6400c04daf17672252707470e6cc65e9b
bengrannan <bengrannan@gmail.com>
2021-10-31 21:09:39 -0700

    added series_to_nifti conversion method to Study class

--- study.py
+++ study.py
@@ -3,6 +3,9 @@ DESCRIPTION = "study class"
 import os
 from glob import glob
 import pydicom
+import numpy as np
+import shutil
+import dicom2nifti
 
 
 class Study(): 
@@ -11,22 +14,52 @@ class Study():
 		self._set_path(path)
 		self._check_depth()
 		self._set_dcm_files()
-		self.descriptions = None
+		self.metadata = None
+		self.seriesDescriptions = None
 
 	def process_study(self):
 		return None
 
-	def set_series_descriptions(self):
-		descriptions = {}
-		for f in self.dcm_files:
+	def read_all_metadata(self):
+		metadata = {}
+		for n, f in enumerate(self.dcm_files):
+			meta_ = {}
 			dcm_ = pydicom.read_file(f)
-			descriptions[f] = dcm_.StudyDescription
-		self.descriptions = descriptions
+			meta_['StudyDate'] = dcm_.StudyDate
+			meta_['Manufacturer'] = dcm_.Manufacturer
+			meta_['StudyDescription'] = dcm_.StudyDescription
+			meta_['SeriesDescription'] = dcm_.SeriesDescription
+			meta_['StudyID'] = dcm_.StudyID 
+			meta_['SeriesNumber'] = dcm_.SeriesNumber
+			meta_['AcquisitionNumber'] = dcm_.AcquisitionNumber
+			meta_['InstanceNumber'] = dcm_.InstanceNumber
+			meta_['PixelSpacing'] = dcm_.PixelSpacing
+			meta_['WindowCenter'] = dcm_.WindowCenter
+			meta_['WindowWidth'] = dcm_.WindowWidth
+			metadata[f] = meta_
+			print('\r{}% complete'.format(np.round(n/len(self.dcm_files)*100,2)), end='')
+		self.metadata = metadata
+		self._set_series_names()
 
-	def get_series_descriptions(self):
-		if self.descriptions is None:
+	def get_series_names(self):
+		if self.seriesDescriptions is None:
 			self.set_series_descriptions()
-		return self.descriptions
+		return set(list(self.seriesDescriptions.keys()))
+
+	def convert_series_to_nifti(self, series_description, output_path=None, output_file='new_nifti.nii'):
+		assert series_description in self.get_series_names(), "The series specified does not match a series description"
+		tmp_path = os.path.join(os.path.split(self.path)[0], 'tmp')
+		if output_path is None:
+			output_path = os.path.split(self.path)[0]
+		if os.path.isdir(tmp_path):
+			assert len(os.path.listdir(tmp_path)) == 0, "Tmp folder should be empty"
+		else:
+			os.mkdir(tmp_path)
+		for f in self.seriesDescriptions[series_description]:
+			shutil.copy(f, tmp_path)
+		dicom2nifti.dicom_series_to_nifti(tmp_path, os.path.join(output_path, output_file))
+		os.rmdir(tmp_path)
+		print('File: {} created.'.format(os.path.join(output_path, output_file)))
 
 	def _set_path(self, path):
 		assert os.path.isdir(path), "Specified path is not a directory"
@@ -41,7 +74,23 @@ class Study():
 				directories only containing single files.".format(f)
 
 	def _set_dcm_files(self):
+		# creates list of dicom files
 		self.dcm_files = glob(os.path.join(self.path, "*/*"))
+
+	def _set_series_names(self):
+		series = {}
+		for f, meta_ in self.metadata.items():
+			seriesDescription_ = meta_['SeriesDescription']
+			if seriesDescription_ not in series.keys():
+				series[seriesDescription_] = [f]
+			else:
+				series[seriesDescription_].append(f)
+		self.seriesDescriptions = series
+
+
+
+
+	
 
 
 
