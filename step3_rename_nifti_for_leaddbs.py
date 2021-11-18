b1c02632eb7f301f974a17f6ab8714924528aba9
bengrannan <bengrannan@gmail.com>
2021-11-18 05:55:58 -0800

    updating .gitignore to remove local bash scripts

---
+++ step3_rename_nifti_for_leaddbs.py
@@ -0,0 +1,4 @@
+DESCRIPTION="script to choose nifti files and reform per lead dbs convention"
+
+# This is currently done manually
+# Text file is made in each LeadDBS directory specifying which nifti files are renamed
